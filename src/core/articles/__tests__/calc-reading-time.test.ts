3ee1e785747efd6e19e8db4e271d311c6361ab3c
cgombauld <clement.gombauld@cpexterne.org>
2022-11-20 13:24:43 +0100

    refacto to pipe

--- src/core/articles/__tests__/calc-reading-time.test.ts
+++ src/core/articles/__tests__/calc-reading-time.test.ts
@@ -2,8 +2,20 @@ import { calcReadingTime } from "../calc-reading-time";
 
 const content = [{ id: "1", type: "h1", children: [{ text: "word" }] }];
 
+const buildPhrase = (numberOfWord: number) => "word ".repeat(numberOfWord);
+
+const buildSimpleContent = (text: string) => [
+  { id: "1", type: "h1", children: [{ text }] },
+];
+
 describe("calculate the reading time of an article", () => {
   it("should be 1 min to read an article when there is less than 238 words in the article", () => {
     expect(calcReadingTime(content)).toBe("1 min read");
+  });
+
+  it("should be 2 min to read an article when there is 476 words in an article", () => {
+    expect(calcReadingTime(buildSimpleContent(buildPhrase(475)))).toBe(
+      "2 min read"
+    );
   });
 });
--- src/core/articles/calc-reading-time.ts
+++ src/core/articles/calc-reading-time.ts
@@ -1,1 +1,23 @@
-export const calcReadingTime = (content: any) => "1 min read";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { pipe } from "ramda";
+
+const WORD_READ_PER_MIN = 238;
+
+const splitBySpace = (allWords: string): string[] => allWords.split(" ");
+
+const calcRawReadingTime = (allWordsArr: string[]): number =>
+  allWordsArr.length / WORD_READ_PER_MIN;
+
+const insertReadingTimeInTemplate = (readingTime: number): string =>
+  `${readingTime} min read`;
+
+const calcReadingTimeOperations = pipe(
+  splitBySpace,
+  calcRawReadingTime,
+  Math.ceil,
+  insertReadingTimeInTemplate
+);
+
+export const calcReadingTime = (content: any[]) => {
+  return calcReadingTimeOperations(content[0].children[0].text);
+};
