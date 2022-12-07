575842f1243ae2cd08cdd9f8b4161f64650f823c
cgombauld <clement.gombauld@cpexterne.org>
2022-12-07 08:44:15 +0100

    added basic middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
 import dotenv from "dotenv";
+import express from "express";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
 
 dotenv.config({ path: "./.env" });
 
-console.log(process.env.PORT);
+const app = express();
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  })
+);
+
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+
+app.use("/api", limiter);
+app.use(express.json({ limit: "100kb" }));
+app.use(express.urlencoded({ extended: true, limit: "100kb" }));
+app.use(mongoSanitize());
+app.use(xss());
+app.use((req, res, next) => {
+  console.log("Hello from the middleware");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
