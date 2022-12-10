15f911b48ea43c3e6c9ef4e1fb01b914096e2592
cgombauld <clement.gombauld@cpexterne.org>
2022-12-10 19:36:29 +0100

    fixed cors problem

--- src/index.ts
+++ src/index.ts
@@ -40,8 +40,8 @@ app.use(express.json({ limit: "100kb" }));
 app.use(express.urlencoded({ extended: true, limit: "100kb" }));
 app.use(mongoSanitize());
 app.use(xss());
-app.use((req, res: any, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH"
@@ -50,7 +50,7 @@ app.use((req, res: any, next) => {
     "Access-Control-Allow-Headers",
     "Content-Type, Accept, Authorization, Origin, X-Auth-Token, X-Requested-With"
   );
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Credentials", "include");
   next();
 });
 
