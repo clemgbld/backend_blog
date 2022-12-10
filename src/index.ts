3c9465b73e400f029950a20ed1492b2a5800f5c0
cgombauld <clement.gombauld@cpexterne.org>
2022-12-10 18:38:24 +0100

    solving cors error

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,8 @@ app.use(xss());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type Accept");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
