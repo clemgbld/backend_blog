8853551c74e7286ce9442ef6deb52f78e8cea185
cgombauld <clement.gombauld@cpexterne.org>
2023-04-10 13:02:18 +0200

    used reuable fixture for email template

--- src/app/subscription/__tests__/subscription-controllers.test.ts
+++ src/app/subscription/__tests__/subscription-controllers.test.ts
@@ -1,9 +1,10 @@
-// import express, { Express } from "express";
-// import request from "supertest";
-// import bodyParser from "body-parser";
+import express, { Express } from "express";
+import request from "supertest";
+import bodyParser from "body-parser";
+import { EmailService } from "../../../core/subscription/domain/gateway/email-service";
+import { SubscriptionRepository } from "../../../core/subscription/domain/repositories/subscription-repository";
+import { FilesRepository } from "../../../core/subscription/domain/repositories/files-repository";
 
-// import { EmailService } from "../../../core/subscription/domain/gateway/email-service";
-
-// describe("notify subscriberes controller", () => {
-//   it("should get a 201 response", async () => {});
-// });
+describe("notify subscriberes controller", () => {
+  it("should get a 201 response", async () => {});
+});
---
+++ src/core/subscription/fixtures/email-template.ts
@@ -0,0 +1,514 @@
+export const emailTemplate = `<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0;" />
+  <meta name="format-detection" content="telephone=no" />
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      min-width: 100%;
+      width: 100% !important;
+      height: 100% !important;
+    }
+    body,
+    table,
+    td,
+    div,
+    p,
+    a {
+      -webkit-font-smoothing: antialiased;
+      text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+      -webkit-text-size-adjust: 100%;
+      line-height: 100%;
+    }
+    table,
+    td {
+      border-collapse: collapse !important;
+      border-spacing: 0;
+    }
+    img {
+      border: 0;
+      line-height: 100%;
+      outline: none;
+      text-decoration: none;
+      -ms-interpolation-mode: bicubic;
+    }
+    #outlook a {
+      padding: 0;
+    }
+    .ReadMsgBody {
+      width: 100%;
+    }
+    .ExternalClass {
+      width: 100%;
+    }
+    .ExternalClass,
+    .ExternalClass p,
+    .ExternalClass span,
+    .ExternalClass font,
+    .ExternalClass td,
+    .ExternalClass div {
+      line-height: 100%;
+    }
+    @media all and (min-width: 560px) {
+      .container {
+        border-radius: 8px;
+        -webkit-border-radius: 8px;
+        -moz-border-radius: 8px;
+        -khtml-border-radius: 8px;
+      }
+    }
+    a,
+    a:hover {
+      color: #127db3;
+    }
+    .footer a,
+    .footer a:hover {
+      color: #999999;
+    }
+  </style>
+
+  <title>Get this responsive email template</title>
+</head>
+<body
+  topmargin="0"
+  rightmargin="0"
+  bottommargin="0"
+  leftmargin="0"
+  marginwidth="0"
+  marginheight="0"
+  width="100%"
+  style="
+    border-collapse: collapse;
+    border-spacing: 0;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    -webkit-font-smoothing: antialiased;
+    text-size-adjust: 100%;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    line-height: 100%;
+    background-color: #212529;
+    color: #000000;
+  "
+  bgcolor="#212529"
+  text="#000000"
+>
+  <table
+    width="100%"
+    align="center"
+    border="0"
+    cellpadding="0"
+    cellspacing="0"
+    style="
+      border-collapse: collapse;
+      border-spacing: 0;
+      margin: 0;
+      padding: 0;
+      width: 100%;
+    "
+    class="background"
+  >
+    <tr>
+      <td
+        align="center"
+        valign="top"
+        style="
+          border-collapse: collapse;
+          border-spacing: 0;
+          margin: 0;
+          padding: 0;
+        "
+        bgcolor="#212529"
+      >
+        <table
+          border="0"
+          cellpadding="0"
+          cellspacing="0"
+          align="center"
+          width="560"
+          style="
+            border-collapse: collapse;
+            border-spacing: 0;
+            padding: 0;
+            width: inherit;
+            max-width: 560px;
+          "
+          class="wrapper"
+        >
+          <tr>
+            <td
+              align="center"
+              valign="top"
+              style="
+                border-collapse: collapse;
+                border-spacing: 0;
+                margin: 0;
+                padding: 0;
+                padding-left: 6.25%;
+                padding-right: 6.25%;
+                width: 87.5%;
+                padding-top: 20px;
+                padding-bottom: 20px;
+              "
+            >
+              <div
+                style="
+                  display: none;
+                  visibility: hidden;
+                  overflow: hidden;
+                  opacity: 0;
+                  font-size: 1px;
+                  line-height: 1px;
+                  height: 0;
+                  max-height: 0;
+                  max-width: 0;
+                  color: #212529;
+                "
+                class="preheader"
+              ></div>
+              <a
+                target="_blank"
+                style="text-decoration: none"
+                href="https://blog-clement-gombauld.vercel.app/"
+                ><img
+                  border="0"
+                  vspace="0"
+                  hspace="0"
+                  src="https://blog-clement-gombauld.vercel.app/_next/image?url=%2Fapp-logo.png&w=1080&q=75"
+                  width="90"
+                  height="87"
+                  alt="Logo"
+                  title="Logo"
+                  style="
+                    object-fit: cover;
+                    color: #000000;
+                    font-size: 10px;
+                    margin: 0;
+                    padding: 0;
+                    outline: none;
+                    text-decoration: none;
+                    -ms-interpolation-mode: bicubic;
+                    border: none;
+                    display: block;
+                  "
+              /></a>
+            </td>
+          </tr>
+        </table>
+        <table
+          border="0"
+          cellpadding="0"
+          cellspacing="0"
+          align="center"
+          bgcolor="#212529"
+          color="#c5f6fa"
+          width="560"
+          style="
+            border-collapse: collapse;
+            border-spacing: 0;
+            padding: 0;
+            width: inherit;
+            max-width: 560px;
+          "
+          class="container"
+        >
+          <tr>
+            <td
+              align="center"
+              valign="top"
+              style="
+                border-collapse: collapse;
+                border-spacing: 0;
+                margin: 0;
+                padding: 0;
+                padding-left: 6.25%;
+                padding-right: 6.25%;
+                width: 87.5%;
+                font-size: 24px;
+                font-weight: bold;
+                line-height: 130%;
+                padding-top: 25px;
+                color: #08fdd8;
+                font-family: sans-serif;
+              "
+              class="header"
+            >
+              A new Article has been published!
+            </td>
+          </tr>
+          <tr>
+            <td
+              align="center"
+              valign="top"
+              style="
+                border-collapse: collapse;
+                border-spacing: 0;
+                margin: 0;
+                padding: 0;
+                padding-bottom: 3px;
+                padding-left: 6.25%;
+                padding-right: 6.25%;
+                width: 87.5%;
+                font-size: 18px;
+                font-weight: 300;
+                line-height: 150%;
+                padding-top: 5px;
+                color: #08fdd8;
+                font-family: sans-serif;
+              "
+              class="subheader"
+            >
+              #TITLE
+            </td>
+          </tr>
+          <tr>
+            <td
+              align="center"
+              valign="top"
+              style="
+                border-collapse: collapse;
+                border-spacing: 0;
+                margin: 0;
+                padding: 0;
+                padding-top: 20px;
+              "
+              class="hero"
+            >
+              <a
+                target="_blank"
+                style="text-decoration: none"
+                href="https://blog-clement-gombauld.vercel.app/#ID"
+                ><img
+                  border="0"
+                  vspace="0"
+                  hspace="0"
+                  src="#IMG_SRC"
+                  alt="#TITLE"
+                  width="560"
+                  style="
+                    object-fit: cover;
+                    width: 100%;
+                    max-width: 560px;
+                    color: #000000;
+                    font-size: 13px;
+                    margin: 0;
+                    padding: 0;
+                    outline: none;
+                    text-decoration: none;
+                    -ms-interpolation-mode: bicubic;
+                    border: none;
+                    display: block;
+                  "
+              /></a>
+            </td>
+          </tr>
+          <tr>
+            <td
+            align="center"
+            valign="top"
+            style="
+              border-collapse: collapse;
+              border-spacing: 0;
+              margin: 0;
+              padding: 0;
+              padding-left: 6.25%;
+              padding-right: 6.25%;
+              width: 87.5%;
+              font-size: 17px;
+              font-weight: 400;
+              line-height: 160%;
+              padding-top: 25px;
+              color: #c5f6fa;
+              font-family: sans-serif;
+            "
+            class="paragraph"
+          >
+            Topic: #TOPIC
+          </td>
+          </tr>
+          <tr>
+            <td
+            align="center"
+            valign="top"
+            style="
+              border-collapse: collapse;
+              border-spacing: 0;
+              margin: 0;
+              padding: 0;
+              padding-left: 6.25%;
+              padding-right: 6.25%;
+              width: 87.5%;
+              font-size: 17px;
+              font-weight: 400;
+              line-height: 160%;
+              padding-top: 25px;
+              color: #c5f6fa;
+              font-family: sans-serif;
+            "
+            class="paragraph"
+          >
+            Time to read: #TIME_TO_READ
+          </td>
+          </tr>
+          <tr>
+            
+            <td
+              align="center"
+              valign="top"
+              style="
+                border-collapse: collapse;
+                border-spacing: 0;
+                margin: 0;
+                padding: 0;
+                padding-left: 6.25%;
+                padding-right: 6.25%;
+                width: 87.5%;
+                font-size: 17px;
+                font-weight: 400;
+                line-height: 160%;
+                padding-top: 25px;
+                color: #c5f6fa;
+                font-family: sans-serif;
+              "
+              class="paragraph"
+            >
+              #SUMMARY
+            </td>
+          </tr>
+          <tr>
+            <td
+              align="center"
+              valign="top"
+              style="
+                border-collapse: collapse;
+                border-spacing: 0;
+                margin: 0;
+                padding: 0;
+                padding-left: 6.25%;
+                padding-right: 6.25%;
+                width: 87.5%;
+                padding-top: 25px;
+                padding-bottom: 5px;
+              "
+              class="button"
+            >
+              <a
+                href="https://blog-clement-gombauld.vercel.app/#ID"
+                target="_blank"
+                style="text-decoration: underline"
+              >
+                <table
+                  border="0"
+                  cellpadding="0"
+                  cellspacing="0"
+                  align="center"
+                  style="
+                    max-width: 240px;
+                    min-width: 120px;
+                    border-collapse: collapse;
+                    border-spacing: 0;
+                    padding: 0;
+                  "
+                >
+                  <tr>
+                    <td
+                      align="center"
+                      valign="middle"
+                      style="
+                        padding: 12px 24px;
+                        margin: 0;
+                        text-decoration: underline;
+                        border-collapse: collapse;
+                        border-spacing: 0;
+                        border-radius: 4px;
+                        -webkit-border-radius: 4px;
+                        -moz-border-radius: 4px;
+                        -khtml-border-radius: 4px;
+                      "
+                      bgcolor="#08fdd8"
+                    >
+                      <a
+                        target="_blank"
+                        style="
+                          text-decoration: underline;
+                          color: #000000;
+                          font-family: sans-serif;
+                          font-size: 17px;
+                          font-weight: 400;
+                          line-height: 120%;
+                        "
+                        href="https://blog-clement-gombauld.vercel.app/#ID"
+                      >
+                        Read the article
+                      </a>
+                    </td>
+                  </tr>
+                </table></a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td
+              align="center"
+              valign="top"
+              style="
+                border-collapse: collapse;
+                border-spacing: 0;
+                margin: 0;
+                padding: 0;
+                padding-left: 6.25%;
+                padding-right: 6.25%;
+                width: 87.5%;
+                padding-top: 25px;
+              "
+              class="line"
+            >
+              <hr
+                color="#E0E0E0"
+                align="center"
+                width="100%"
+                size="1"
+                noshade
+                style="margin: 0; padding: 0"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td
+              align="center"
+              valign="top"
+              style="
+                border-collapse: collapse;
+                border-spacing: 0;
+                margin: 0;
+                padding: 0;
+                padding-left: 6.25%;
+                padding-right: 6.25%;
+                width: 87.5%;
+                font-size: 13px;
+                font-weight: 400;
+                line-height: 150%;
+                padding-top: 20px;
+                padding-bottom: 20px;
+                color: #c5f6fa;
+              "
+              class="footer"
+            >
+              If you want to unsubscribe yourself from this newsletter, please
+              send an email to clementgombaulddev@gmail.com.
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
+`
+  .replace(/\s+/g, " ")
+  .trim();
