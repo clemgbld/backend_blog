6444518723005ceb7564edf0003b9bad161c5373
cgombauld <clement.gombauld@cpexterne.org>
2023-05-08 14:34:51 +0200

    refacto updateArticle to use pipeline

--- src/core/articles/domain/services/parse-article-content.ts
+++ src/core/articles/domain/services/parse-article-content.ts
@@ -1,6 +1,17 @@
+import { ifElse } from "ramda";
 import { ArticleWithStringifyContent } from "../repositories/articles-repository";
 
 export const parseArticleContent = (article: ArticleWithStringifyContent) => ({
   ...article,
   content: JSON.parse(article.content),
 });
+
+export const buildThrowOrParseContent = (message: string) =>
+  ifElse(
+    (article: ArticleWithStringifyContent | undefined) => !article,
+    () => {
+      throw new Error(message);
+    },
+    (article: ArticleWithStringifyContent | undefined) =>
+      parseArticleContent(article as ArticleWithStringifyContent)
+  );
--- src/core/articles/use-cases/update-article.ts
+++ src/core/articles/use-cases/update-article.ts
@@ -1,7 +1,8 @@
+import { andThen, pipe } from "ramda";
 import { buildArticle } from "../domain/articles";
 import { ArticlesRepository } from "../domain/repositories/articles-repository";
 import { stringifyArticleContent } from "../domain/services/stringify-article-content";
-import { parseArticleContent } from "../domain/services/parse-article-content";
+import { buildThrowOrParseContent } from "../domain/services/parse-article-content";
 import { ArticleIn } from "../../../app/articles/dto/article-in";
 
 type UpdateArticle = {
@@ -12,16 +13,10 @@ type UpdateArticle = {
 export const updateArticle = async ({
   articleIn,
   articlesRepository,
-}: UpdateArticle) => {
-  const article = buildArticle(articleIn);
-
-  const modifiedArticle = await articlesRepository.update(
-    stringifyArticleContent(article)
-  );
-
-  if (!modifiedArticle) {
-    throw new Error(`${article.id} id does not exist`);
-  }
-
-  return parseArticleContent(modifiedArticle);
-};
+}: UpdateArticle) =>
+  pipe(
+    buildArticle,
+    stringifyArticleContent,
+    articlesRepository.update,
+    andThen(buildThrowOrParseContent(`${articleIn.id} id does not exist`))
+  )(articleIn);
